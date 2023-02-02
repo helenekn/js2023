3decca75e2d3af2ccd17714403f34377167c0957
Helen <redkina2508@gmail.com>
2023-02-02 23:54:50 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// "use strict";
+
+let incr = 10,
+    decr = 10;
+
+// ++incr;
+// --decr;
+
+console.log(++incr);
+console.log(--decr);
+
+const isChecked = false,
+    isClose = false;
+    
+
+console.log(isChecked || !isClose);   
+
